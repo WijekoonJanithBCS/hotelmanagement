14fe3fcc35194a2a15f0c419e09da0d36e3d799c
WijekoonJanithBCS <wijekoonjanith72@gmail.com>
2025-07-22 18:50:18 +0530

    Initial commit

---
+++ dbcon.php
@@ -0,0 +1,8 @@
+<?php
+
+$host='localhost';
+$un='root';
+$pw='';
+$dbname='hotelmgt';
+
+$con = mysqli_connect($host, $un, $pw, $dbname) or die("failed to connect with DB");
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>TRAVEL LK</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light justify-content-between-between mx-3">
+        <a href="./index.php" class="navbar-brand">Travel LK</a>
+        <a href="./index.php" class="btn btn-success btn-sm">LOG IN</a>
+    </nav>
+
+    <main class="my-5">
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">WEL COME TO CENTRAL PROVINCE</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam cumque minus eos dolore praesentium vero, illum voluptatum at ipsa nesciunt et tempore porro nam veritatis itaque? Hic autem molestiae voluptate?</p>
+            </div>
+        </section>
+
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" style="height:200px;" src="./img/img01.jpg">
+                    <div class="card-body">
+                        <P class="card text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas possimus, magni animi necessitatibus eligendi, expedita natus eveniet voluptates impedit velit repellendus illo id, non asperiores itaque ratione modi a porro. </P>
+                        <div class="d-flex justify-content-between align-items-center">
+                            
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-outline-primary btn-sm">VIEW</button>
+                                <button type="button" class="btn btn-outline-success btn-sm">BOOK NOW</button>
+                            </div>
+                            <small class="text muted">RS.15000/=</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" style="height:200px;" src="./img/img02.jpg">
+                    <div class="card-body">
+                        <P class="card text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas possimus, magni animi necessitatibus eligendi, expedita natus eveniet voluptates impedit velit repellendus illo id, non asperiores itaque ratione modi a porro. </P>
+                        <div class="d-flex justify-content-between align-items-center">
+                            
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-outline-primary btn-sm">VIEW</button>
+                                <button type="button" class="btn btn-outline-success btn-sm">BOOK NOW</button>
+                            </div>
+                            <small class="text muted">RS.15000/=</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" style="height:200px;" src="./img/img03.jpg">
+                    <div class="card-body">
+                        <P class="card text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas possimus, magni animi necessitatibus eligendi, expedita natus eveniet voluptates impedit velit repellendus illo id, non asperiores itaque ratione modi a porro. </P>
+                        <div class="d-flex justify-content-between align-items-center">
+                            
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-outline-primary btn-sm">VIEW</button>
+                                <button type="button" class="btn btn-outline-success btn-sm">BOOK NOW</button>
+                            </div>
+                            <small class="text muted">RS.15000/=</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+            
+
+</body>
+
+</html>
